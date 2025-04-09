d1b45ca7704b0b32c75ebd065c0d2378501b402e
Alex X <dowsha@yandex.ru>
2025-04-09 10:22:18 +0300

    Интегрировал сервис mockapi.io, добавил новые зависимости и конфиг для ruff

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,10 +1,26 @@
+import os
+
+import requests
+from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
-from task_file import TaskFile
+load_dotenv()
 
+api_key = os.getenv("API_KEY")
 
 app = FastAPI()
+
+url = 'https://api.jsonbin.io/v3/b/'
+
+headers = {
+    "X-Master-Key": api_key
+}
+
+headers_type = {
+  'Content-Type': 'application/json',
+  'X-Master-Key': api_key
+}
 
 
 class Task(BaseModel):
@@ -12,49 +28,61 @@ class Task(BaseModel):
     status: str
 
 
-@app.get("/tasks")
-def get_tasks():
-    tasks = TaskFile.read_tasks_from_file()
-    return tasks
+# id таски для примера 67f554708960c979a580c3fb
+# id таски для примера 67f554518561e97a50fb31fd
+@app.get("/tasks/{task_id}")
+def get_tasks(task_id: str):
+    response = requests.get(f'{url}{task_id}', json=None, headers=headers)
+
+    if response.status_code == 200:
+        return response.json()
+    raise HTTPException(
+        status_code=response.status_code, detail="Не удалось получить задачи!"
+    )
 
 
 @app.post("/tasks")
 def create_task(task: Task):
-    tasks = TaskFile.read_tasks_from_file()
-    new_task = {
-        "id": len(tasks) + 1,
+    data = {
         "title": task.title,
         "status": task.status
     }
-    tasks.append(new_task)
-    TaskFile.write_tasks_to_file(tasks)
-    return new_task
+    response = requests.post(url, json=data, headers=headers_type)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise HTTPException(
+            status_code=response.status_code,
+            detail=f"Не удалось создать задачу. Причина: {response.text}"
+        )
 
 
 @app.put("/tasks/{task_id}")
-def update_task(task_id: int, task: Task):
-    tasks = TaskFile.read_tasks_from_file()
-    for x in tasks:
-        if x["id"] == task_id:
-            x["title"] = task.title
-            x["status"] = task.status
-            TaskFile.write_tasks_to_file(tasks)
-            return x
-    raise HTTPException(status_code=404, detail="Таска не найдена!")
+def update_task(task_id: str, task: Task):
+    data = {
+        "title": task.title,
+        "status": task.status
+    }
+    response = requests.put(f'{url}{task_id}', json=data, headers=headers_type)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise HTTPException(
+            status_code=response.status_code,
+            detail=f"Не удалось изменить задачу. Причина: {response.text}"
+        )
 
 
 @app.delete("/tasks/{task_id}")
-def delete_task(task_id: int):
-    tasks = TaskFile.read_tasks_from_file()
-    task_to_delete = None
-    for x in tasks:
-        if x["id"] == task_id:
-            task_to_delete = x
-            break
+def delete_task(task_id: str):
+    response = requests.delete(f'{url}{task_id}', json=None, headers=headers)
 
-    if task_to_delete:
-        tasks.remove(task_to_delete)
-        TaskFile.write_tasks_to_file(tasks)
-        return {"сообщение": "Таска удалена!"}
-
-    raise HTTPException(status_code=404, detail="Таска не найдена!")
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise HTTPException(
+            status_code=response.status_code,
+            detail=f"Не удалось удалить задачу. Причина: {response.text}"
+        )
